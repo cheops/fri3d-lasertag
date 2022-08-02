6be2e07bd3d4d73cecbbe5a20fd2a346c9b90f60
cheops <joram.agten@gmail.com>
2022-08-02 12:27:23 +0200

    boot.py renamed to main.py

--- init_flash.sh
+++ init_flash.sh
@@ -2,4 +2,4 @@ mpremote resume fs cp src/* :
 mpremote resume fs cp Timeblaster/MicroPython/blaster.py :
 mpremote resume fs cp st7789py_mpy/lib/st7789py.py :
 
-mpremote resume run src/boot.py
+mpremote resume run src/main.py
