d655eb482bc991cb1993093302d27d80a0ae4f87
cheops <joram.agten@gmail.com>
2022-08-09 13:10:48 +0200

    manifest to freeze

---
+++ fri3d-lasertag-manifest.py
@@ -0,0 +1,23 @@
+files = [
+    "src/booting_screen.py",
+    "src/chango_16.py",
+    "src/chango_32.py",
+    "src/chango_64.py",
+    "src/display.py",
+    "src/effects.py",
+    "src/fri3d_logo.py",
+    "src/hardware.py",
+    "src/main.py",
+    "src/monitor_countdown.py",
+    "src/monitor_ir.py",
+    "src/mvp.py",
+    "src/profiles_common.py",
+    "src/profiles_mvp.py",
+    "src/statemachine.py",
+    "src/teams.py",
+    "src/vga1_16x32.py",
+    "Timeblaster/MicroPython/blaster.py"
+    "micropython-mqtt/mqtt-as/mqtt_as.py"
+]
+
+freeze("fri3d-lasertag", files)
