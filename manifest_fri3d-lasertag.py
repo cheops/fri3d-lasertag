9c091ba1ee0f11399d229170fda0514cebc24f46
cheops <joram.agten@gmail.com>
2022-08-09 16:25:54 +0200

    put files in root

--- manifest_fri3d-lasertag.py
+++ manifest_fri3d-lasertag.py
@@ -1,24 +1,24 @@
 files = [
-    "src/booting_screen.py",
-    "src/chango_16.py",
-    "src/chango_32.py",
-    "src/chango_64.py",
-    "src/display.py",
-    "src/effects.py",
-    "src/fri3d_logo.py",
-    "src/hardware.py",
-    "src/main.py",
-    "src/monitor_countdown.py",
-    "src/monitor_ir.py",
-    "src/mvp.py",
-    "src/profiles_common.py",
-    "src/profiles_mvp.py",
-    "src/statemachine.py",
-    "src/teams.py",
-    "src/vga1_16x32.py",
-    "Timeblaster/MicroPython/blaster.py"
+    "booting_screen.py",
+    "chango_16.py",
+    "chango_32.py",
+    "chango_64.py",
+    "display.py",
+    "effects.py",
+    "fri3d_logo.py",
+    "hardware.py",
+    "main.py",
+    "monitor_countdown.py",
+    "monitor_ir.py",
+    "mvp.py",
+    "profiles_common.py",
+    "profiles_mvp.py",
+    "statemachine.py",
+    "teams.py",
+    "vga1_16x32.py",
 ]
 
-freeze(".", files)
+freeze("src", files)
+freeze("Timeblaster/MicroPython", "blaster.py")
 freeze("micropython-mqtt/mqtt_as", "mqtt_as.py")
 
